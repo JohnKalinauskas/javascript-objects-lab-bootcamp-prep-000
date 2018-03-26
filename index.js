474e74704aa9df3711082b2077056cc265e0c0aa
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 16:30:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+ return Object.assign({}, obj, { [key]: value })
+
+}
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+  return obj
+}
+
+function deleteFromObjectByKey(object,key) { 
+    delete object[key];
+    return Object.assign({},object,{});
+    
+}
+
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object) 
+  delete object[key]
+  return object
+}
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key]
+return object
+}
