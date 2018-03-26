4c99b010dc6fb2485c72da088b2d7836dfe67dfc
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 16:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 
 function deleteFromObjectByKey(object,key) { 
     delete object[key];
-    return Object
+    return object
     
 }
 
